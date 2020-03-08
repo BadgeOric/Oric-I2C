9a0748a9abde4307db9baa48cbd3b7be053b22f7
BadgeOric <43514521+BadgeOric@users.noreply.github.com>
2020-03-08 09:19:52 +0000

    Add files via upload

--- socket_serverv3/socket_serverv3.ino
+++ socket_serverv3/socket_serverv3.ino
@@ -1,37 +1,28 @@
 #include <ESP8266WiFi.h>
 #include <Wire.h>
-
 int incomingByte = 0; // for incoming serial data
-int bufflen = 0;// count of serial inputs
 int maxbytes=4;
-String rxbuff;
+byte rxbuff[200];
 int rxcount=0;
 byte txbuff[32000];
 byte xbuff[4];
 int txcount=0;
 int curpos=0;
-int first=0;
-const char* ssid = "Wifi SSID HERE";
-const char* password =  "WIFI PASSWORD HERE";
-char mysend;
-String sendstring;
-WiFiServer wifiServer(80);
 
+WiFiServer wifiServer(80);
 void setup() {
+  const char* ssid = "VM4705704";
+  const char* password =  "xdTcwwgW3gbg";
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register events
   Wire.onRequest(requestEvent);
   Serial.begin(9600);
- 
   delay(1000);
-  
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
- 
   Serial.println("Connected to the WiFi network");
   Serial.println(WiFi.localIP());
   Wire.write("Network Connected");
@@ -41,47 +32,35 @@ void setup() {
 void loop() {
  
   WiFiClient client = wifiServer.available();
- 
-  if (client) {
+   if (client) {
         Serial.println("Client Connected \r\n");
+        client.write("Connected to Oric \r\n");
         while (client.connected()) 
         {
-          if (Serial.available()>0) 
-              {
-                incomingByte=Serial.read();
-                client.write(incomingByte);
-                //for (int i=0;i<32;i++)
-                //{
-               // Serial.printf("%d,",txbuff[i]);
-               // }
-              }
-  
-         while (client.available()>0) 
+          while (client.available()>0) 
               {
                 byte s=client.read();
                 txbuff[txcount] = s;
-                
                 txcount++;
               }
           
 
         if (rxcount!=0)
         {
-              //Serial.write(rxbuff);
-             client.write(rxbuff.c_str());
-            rxbuff="";
+              for (int i=0; i<=rxcount;i++)
+              {
+              client.write(rxbuff[i]);
+              }
             rxcount=0;
         } 
-        
-       delay(10);
+        delay(10);
   }
+    
+    client.write("Disconnected from Oric");
     client.stop();
     Serial.println("Client disconnected");
-    curpos=0;
     txcount=0;
     rxcount=0;
-    first=0;
-    rxbuff="";
     Wire.flush();
   }
 }
@@ -90,14 +69,15 @@ void receiveEvent(int howMany)
 {
   for (int i=0;i<howMany;i++)
   {
-  char rx=Wire.read();
-  rxbuff=rxbuff+rx;
+  byte s=Wire.read();
+  rxbuff[rxcount] = s;
   rxcount++;
   }
 }
 
 void requestEvent()
 { 
+     
      if(txcount>0)
      {
             if(txcount<=maxbytes)
@@ -125,6 +105,4 @@ void requestEvent()
      curpos=0;
      }
 
-     
-  
 }
